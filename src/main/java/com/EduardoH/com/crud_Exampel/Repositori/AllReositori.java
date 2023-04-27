8391bfdb28e3a817f08e69c0cc91430c0b3a399d
Eduardo <edua.24h@gmail.com>
2023-04-27 17:02:51 +0100

    add deoendici

---
+++ src/main/java/com/EduardoH/com/crud_Exampel/Repositori/AllReositori.java
@@ -0,0 +1,8 @@
+package com.EduardoH.com.crud_Exampel.Repositori;
+
+
+import com.EduardoH.com.crud_Exampel.Model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AllReositori extends JpaRepository <Task,Long> {
+}
